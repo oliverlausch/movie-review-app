07c8f1dd96c852afdf1c953fb2dc8942cadd52f5
AWelshFail <34342302+AWelshFail@users.noreply.github.com>
2022-04-21 14:26:51 +0100

    GitFix

--- COTE-Movies/src/components/Admin.js
+++ COTE-Movies/src/components/Admin.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import GetReviews from './GetReviews';
-<<<<<<< HEAD
 import { makeStyles } from '@material-ui/core/Styles';
 import Navbar from './Navbar';
 import Footer from './Footer';
@@ -10,42 +9,20 @@ const useStyles = makeStyles(theme => ({
         marginTop: "-19px !important"
     }
   }));
-=======
-import GetUsers from './Users/GetUsers';
-import styled from 'styled-components';
-
-const Title = styled.div`
-display: flex;
-margin-left: 13%; 
-align-items: center;
-`;
-
-const Container = styled.div`
-display: flex;
-flex-direction: row;
-padding: 20px;
-justify-content: even;
-border-bottom: 1px solid #969696;
-`;
->>>>>>> becea5c5 (Added User CRUD, update and delete forms, updated Admin Panel)
 
 function Admin() {
     const classes = useStyles();
     return (
         <>
-<<<<<<< HEAD
         <Navbar />
         
         <div className={classes.Admin}>
         <h1>Welcome to the Admin Dashboard</h1>
-=======
-        <Title><h1>Welcome to the Admin Dashboard</h1></Title>
->>>>>>> becea5c5 (Added User CRUD, update and delete forms, updated Admin Panel)
         
-        <Container>
+        {/* <Container>
             <GetUsers />
             <GetReviews /> 
-        </Container>
+        </Container> */}
        
         
         </div>
